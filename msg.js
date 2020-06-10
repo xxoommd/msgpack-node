291c1e2cafe8fc6cf10a6a5bd33ccac37cd2360e
gaohengyi <xxoommd@163.com>
2020-06-10 17:40:05 +0800

    Fix \"\t\" error

--- msg.js
+++ msg.js
@@ -1,5 +1,7 @@
 const msgpack = require('msgpack-lite')
 
+let encoder = new TextEncoder()
+
 function msgDecode(hexStr) {
     let i = 0
     let arr = []
@@ -15,6 +17,9 @@ function msgDecode(hexStr) {
                 word = word.replace('\\x', '0x')
                 arr.push(parseInt(word, 16))
                 i++
+            } else if (hexStr[i + 1] === 't') {
+                i += 2
+                arr.push(0x09)
             } else {
                 char = hexStr[i + 1]
                 let b = encoder.encode(char) // Buffer.from(char)
@@ -30,7 +35,9 @@ function msgDecode(hexStr) {
         if (i >= hexStr.length - 1) break
     }
 
-    return msgpack.decode(Buffer.from(arr))
+    let buf = Buffer.from(arr)
+
+    return msgpack.decode(buf)
 }
 
 // const dataStr = "\\x88\\xa3Uid\\xb5denasand_ioslcm_13842\\xacRealNameSign\\x01\\xa2Id\\xa266\\xaeOnlineTimeData\\x92\\x83\\xa4Date\\xa92020-5-11\\xa8Duration\\xcc\\xe6\\xb0XXX_unrecognized\\xc0\\x83\\xa4Date\\xa92020-5-12\\xa8Duration\\xcd\\x101\\xb0XXX_unrecognized\\xc0\\xa9AuthCount\\xc0\\xa8AuthTime\\xc0\\xafTotalOnlineTime\\xcd\\x11\\x17\\xb0XXX_unrecognized\\xc0"
